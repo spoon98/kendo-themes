4cceb9494ed8384eeb443e2b9e12aa3a10c84521
Paul Johnson <pauljohnson5@deloitte.com>
2019-09-04 13:48:22 -0400

    updated test, removed test except buttons

--- tests/integrity/data/metrics.js
+++ tests/integrity/data/metrics.js
@@ -1,115 +1,113 @@
 var metrics = {
-
     // Button
-    "button": {
-        "height": 30
+    button: {
+        height: 33
     },
-    "iconButton": {
-        "width": 30,
-        "height": 30
+    iconButton: {
+        width: 33,
+        height: 33
     },
 
     // Inputs
-    "autocomplete": {
-        "height": 30
+    autocomplete: {
+        height: 30
     },
-    "colorpicker": {
-        "width": 58,
-        "height": 30,
-        "select": {
-            "width": 28,
-            "height": 28
+    colorpicker: {
+        width: 58,
+        height: 30,
+        select: {
+            width: 28,
+            height: 28
         },
-        "preview": {
-            "width": 28,
-            "height": 28
+        preview: {
+            width: 28,
+            height: 28
         }
     },
-    "combobox": {
-        "height": 30,
-        "select": {
-            "width": 28,
-            "height": 28
+    combobox: {
+        height: 30,
+        select: {
+            width: 28,
+            height: 28
         }
     },
-    "datetime": {
-        "height": 30,
-        "select": {
-            "width": 28,
-            "height": 28
+    datetime: {
+        height: 30,
+        select: {
+            width: 28,
+            height: 28
         }
     },
-    "dropdown": {
-        "height": 30,
-        "select": {
-            "width": 28,
-            "height": 28
+    dropdown: {
+        height: 30,
+        select: {
+            width: 28,
+            height: 28
         }
     },
-    "maskedtextbox": {
-        "height": 30
+    maskedtextbox: {
+        height: 30
     },
-    "multiselect": {
-        "height": 30,
-        "token": {
-            "height": 24
+    multiselect: {
+        height: 30,
+        token: {
+            height: 24
         }
     },
-    "numeric": {
-        "height": 30,
-        "select": {
-            "width": 28,
-            "height": 28
+    numeric: {
+        height: 30,
+        select: {
+            width: 28,
+            height: 28
         }
     },
-    "slider": {
-        "height": 30,
-        "buttons": {
-            "width": 30,
-            "height": 30
+    slider: {
+        height: 30,
+        buttons: {
+            width: 30,
+            height: 30
         },
-        "handle": {
-            "width": 16,
-            "height": 16
+        handle: {
+            width: 16,
+            height: 16
         },
-        "track": {
-            "height": 4
+        track: {
+            height: 4
         }
     },
-    "listbox": {},
-    "upload": {
-        "height": 48,
-        "button": {
-            "height": 30,
-            "minWIdth": 98
+    listbox: {},
+    upload: {
+        height: 48,
+        button: {
+            height: 30,
+            minWIdth: 98
         }
     },
-    "switch": {
-        "width": 60,
-        "height": 30
+    switch: {
+        width: 60,
+        height: 30
     },
 
     // Window
-    "window": {
-        "titlebar": {
-            "height": 45
+    window: {
+        titlebar: {
+            height: 45
         },
-        "actions": {
-            "width": 30,
-            "height": 30
+        actions: {
+            width: 30,
+            height: 30
         }
     },
-    "dialog": {
-        "titlebar": {
-            "height": 45
+    dialog: {
+        titlebar: {
+            height: 45
         },
-        "actions": {
-            "width": 30,
-            "height": 30
+        actions: {
+            width: 30,
+            height: 30
         },
-        "buttons": {
-            "height": 44
+        buttons: {
+            height: 44
         }
     }
-
 };
